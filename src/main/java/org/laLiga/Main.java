1a723c8ba4ce4c018aba86e2f5dfbca536d0dcb2
Michael-Chacon <michaelchacon02@gmail.com>
2024-05-28 20:38:49 -0500

    feat: :sparkles: Organización de menús
    Se organizaron los menús, para que sean más intuitivo.

--- src/main/java/org/laLiga/Main.java
+++ src/main/java/org/laLiga/Main.java
@@ -1,6 +1,8 @@
 package org.laLiga;
 import org.laLiga.controlador.AdministrarEquipo;
 import org.laLiga.modelo.Equipo;
+
+import java.sql.SQLOutput;
 import java.text.ParseException;
 import java.util.List;
 import java.util.Scanner;
@@ -8,17 +10,27 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws ParseException {
         Scanner sc = new Scanner(System.in);
+//        La clase AdministrarEquipo contiene los crud requeridos.
         AdministrarEquipo repo = new AdministrarEquipo();
+
         int control = 0;
 
         principal: while(true){
-//
-            System.out.println("LA LIGA");
+//          Las obtenerEquipos y totalEquipos son variables usadas para validar que existan equipos registrados.
+            List<Equipo> obtenerEquipos = repo.listar();
+            int totalEquipos = obtenerEquipos.size();
+
+            System.out.println("----------");
+            System.out.println("| LA LIGA |");
+            System.out.println("----------");
+
             System.out.println("Options: \n\t1. Registrar equipo\n\t2. Registrar juego\n\t3. Informes\n\t4. Tabla\n\t0. Salir");
             control = sc.nextInt();
 
             if (control == 1){
-                System.out.println("Module for register team:");
+                System.out.println("--------------------------");
+                System.out.println("** Registro de equipos **");
+                System.out.println("--------------------------\n");
                 registro: while(true){
                     int id = repo.ultimoid();
                     System.out.println("Ingrese el nombre del equipo");
@@ -31,37 +43,46 @@ public class Main {
                     }
                 }
             }else if (control == 2){
-                System.out.println("Modulo de registro de partidos");
-                System.out.println("----------------------");
-                System.out.println("| ID \t| NOMBRE");
-                System.out.println("----------------------");
-                List<Equipo> equipos = repo.listar();
-                equipos.forEach(equipo -> {
-                    System.out.println("| " + equipo.getId() + " \t| " + equipo.getNombre());
-                });
-                System.out.println("----------------------");
-                System.out.println();
+                if (totalEquipos <= 1){
+                    System.out.println("\n-------------------------------------------------------");
+                    System.out.println("** No hay suficientes equipos para iniciar el torneo **");
+                    System.out.println("-------------------------------------------------------\n");
 
-                System.out.println("Escriba el id del equipo que jugó de local");
-                int local = sc.nextInt();
-                Equipo equipoLocal = repo.buscarPorId(local);
-                System.out.println("Cuantos goles hizo " + equipoLocal.getNombre()+ ": ");
-                int golesLocal = sc.nextInt();
+                }else {
+                    System.out.println("--------------------------");
+                    System.out.println("** Registro de partidos **");
+                    System.out.println("--------------------------\n");
+                    System.out.println("----------------------");
+                    System.out.println("| ID \t| NOMBRE");
+                    System.out.println("----------------------");
+                    List<Equipo> equipos = repo.listar();
+                    equipos.forEach(equipo -> {
+                        System.out.println("| " + equipo.getId() + " \t| " + equipo.getNombre());
+                    });
+                    System.out.println("----------------------");
+                    System.out.println();
 
-                System.out.println("Escriba el id del equipo que jugó de Visitante");
-                int visitante = sc.nextInt();
-                Equipo equipoVisitante = repo.buscarPorId(visitante);
-                System.out.println("Cuantos goles hizo " + equipoVisitante.getNombre()+ ": ");
-                int golesVisitante = sc.nextInt();
+                    System.out.println("Escriba el id del equipo que jugó de local");
+                    int local = sc.nextInt();
+                    Equipo equipoLocal = repo.buscarPorId(local);
+                    System.out.println("Cuantos goles hizo " + equipoLocal.getNombre()+ ": ");
+                    int golesLocal = sc.nextInt();
 
-                if (golesLocal > golesVisitante || golesLocal == golesVisitante) {
-                    repo.registraCombate(equipoLocal, equipoVisitante, golesLocal, golesVisitante);
-                }else{
-                    repo.registraCombate(equipoVisitante, equipoLocal, golesVisitante, golesLocal);
+                    System.out.println("Escriba el id del equipo que jugó de Visitante");
+                    int visitante = sc.nextInt();
+                    Equipo equipoVisitante = repo.buscarPorId(visitante);
+                    System.out.println("Cuantos goles hizo " + equipoVisitante.getNombre()+ ": ");
+                    int golesVisitante = sc.nextInt();
+
+                    if (golesLocal > golesVisitante || golesLocal == golesVisitante) {
+                        repo.registraCombate(equipoLocal, equipoVisitante, golesLocal, golesVisitante);
+                    }else{
+                        repo.registraCombate(equipoVisitante, equipoLocal, golesVisitante, golesLocal);
+                    }
                 }
-//
             } else if (control == 3) {
                 System.out.println("Modulo de informes");
+
                 informes: while (true){
                     System.out.println("Seleccione el informe que quiere ver:\n\t1. Equipo más goleador\n\t2. Equipo con más Puntos" +
                             "\n\t3. Equipo con más partidos ganados\n\t4. Total de goles anotados por todos\n\t5. Promedio de goles anotados");
@@ -90,19 +111,29 @@ public class Main {
                     }
                 }
             } else if (control == 4){
-                System.out.println("Listado de equipos");
-                List<Equipo> equipos = repo.listar();
-                System.out.println("--------------------------------------------------------------------------------");
-                System.out.println("| ID \t| PJ \t| PG \t| PP \t| PE \t| GF \t| GC \t| TP \t|  NOMBRE \t\t|");
-                System.out.println("--------------------------------------------------------------------------------");
-                equipos.forEach(equipo -> {
-                    System.out.println("| " +equipo.getId() +" \t| "+  equipo.getPj() +" \t| "+ equipo.getPg() +" \t| "+ equipo.getPp() +" \t| "+ equipo.getPe() +" \t| "+ equipo.getGf() +" \t| "+ equipo.getGc() +" \t| "+ equipo.getTp() +" \t| "+equipo.getNombre());
-                });
-                System.out.println("--------------------------------------------------------------------------------");
+                if(totalEquipos == 0){
+                    System.out.println("\n----------------------------------");
+                    System.out.println("** No hay equipos registrados **");
+                    System.out.println("----------------------------------\n");
+                }else {
+                    System.out.println("Listado de equipos");
+                    List<Equipo> equipos = repo.listar();
+                    System.out.println("--------------------------------------------------------------------------------");
+                    System.out.println("| ID \t| PJ \t| PG \t| PP \t| PE \t| GF \t| GC \t| TP \t|  NOMBRE \t\t|");
+                    System.out.println("--------------------------------------------------------------------------------");
+                    equipos.forEach(equipo -> {
+                        System.out.println("| " +equipo.getId() +" \t| "+  equipo.getPj() +" \t| "+ equipo.getPg() +" \t| "+ equipo.getPp() +" \t| "+ equipo.getPe() +" \t| "+ equipo.getGf() +" \t| "+ equipo.getGc() +" \t| "+ equipo.getTp() +" \t| "+equipo.getNombre());
+                    });
+                    System.out.println("--------------------------------------------------------------------------------");
+                }
             }
             else if (control == 0){
                 break;
             }
         }
+        System.out.println("-----------------");
+        System.out.println("** Estás fuera **");
+        System.out.println("-----------------");
+
     }
 }
